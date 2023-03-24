e74baf54c01099255703a6301963934f48ff5641
Parvin Sattorova <nanajon66@gmail.com>
2023-03-23 22:46:15 -0600

    feat: add updates to shelf.js portion of the solo project

--- src/shelf.js
+++ src/shelf.js
@@ -1,18 +1,43 @@
 
 function shelfBook(book, sciFiShelf){
-  for(var i = 0; i < sciFiShelf.length; i++){
-    return  sciFiShelf[i];
-    //sciFiShelf.push(book);
+  if(sciFiShelf.length < 3){
+    return sciFiShelf.unshift(book);
   };
-  //return sciFiShelf;
-
 };
+
+function unshelfBook(bookTitle, sciFiShelf){
+  for(var i =0; i < sciFiShelf.length; i++){
+    if(sciFiShelf[i].title === bookTitle){
+      sciFiShelf.splice(i, 1);
+    };
+  };  
+};
+
+function listTitles(booksInfo){
+  var listOfTitles = [];
+  for(var i = 0; i < booksInfo.length; i++){
+      listOfTitles.push(booksInfo[i].title)
+  };
+  return listOfTitles.join(", ");
+};
+
+function searchShelf(booksOnShelf){
+  console.log(booksOnShelf)
+  for(var i = 0; i < booksOnShelf.length; i++){
+   if(!booksOnShelf.includes(booksOnShelf[i].title)){
+    return true;
+   }else if(booksOnShelf.includes(booksOnShelf[i].title)){
+    return false;
+   };
+  };
+};
+
 
 
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
